d45bf379c4fc4a134f3e86ad75c5a4ef927d83a7
Devendra Korde <devendra.korde@rib-software.com>
2025-06-27 12:37:59 +0530

    service file url updated

--- src/services/todo-service.tsx
+++ src/services/todo-service.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const fetchData = async () => {
-  return await axios.get("http://localhost:5079/todoitems");
+  return await axios.get("http://4.154.193.42:8080/todoitems");
 };
 export const InsertData = async (todoPayload) => {
-  return await axios.post("http://localhost:5079/createtodoitem", todoPayload);
+  return await axios.post(
+    "http://4.154.193.42:8080/createtodoitem",
+    todoPayload
+  );
 };
